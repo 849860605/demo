a466124945949c9a666e4d8a7905d8abed3ee9cf
star <849860605@qq.com>
2022-09-03 21:07:18 +0800

    第一次提交

---
+++ src/main/java/com/star/deno/DenoApplication.java
@@ -0,0 +1,13 @@
+package com.star.deno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenoApplication.class, args);
+	}
+
+}
